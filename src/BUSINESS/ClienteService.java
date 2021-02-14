28329c3bf287fda875577efbd8a9f20783e8d3b4
Camila Duarte <camiladuartes_@ufrn.edu.br>
2021-02-14 16:11:20 -0300

    classe ClienteService + teste na main

---
+++ src/BUSINESS/ClienteService.java
@@ -0,0 +1,77 @@
+package BUSINESS;
+
+public class ClienteService {
+	protected Cliente[] clientes;
+	
+	// Construtores
+	public ClienteService() {
+		
+	}
+	
+	public ClienteService(Cliente[] clientes) {
+		super();
+		this.clientes = clientes;
+	}
+	
+	// Getters e Setters
+	public Cliente[] getCliente() {
+		return clientes;
+	}
+
+	public void setCliente(Cliente[] clientes) {
+		this.clientes = clientes;
+	}
+
+	// Regras de Negócio	
+	public void validarCadastroCliente(int idCliente) {
+		// Checagem se cliente existe
+		Cliente cliente = consultarCliente(idCliente);
+		
+		if(cliente == null) {
+			System.out.println(">> Cliente de ID=" + idCliente + " não cadastrado.");
+		}
+		else {	
+			// Validação de CPF (Padrão: 11 dígitos.)
+			if(cliente.getCpf().length() != 11) {
+				System.out.println(">> CPF do cliente " + cliente.getIdCliente() + " cadastrado incorretamente. "
+						+ "Insira novamente!");
+			}
+			
+			// Validação de Telefone (Padrão: DDD-8números ou DDD-9números. Exemplo: 89-33332222)
+			if((cliente.getTelefone().length() != 11 && 
+			   cliente.getTelefone().length() != 12) ||
+			   cliente.getTelefone().toCharArray()[2] != '-') {
+				System.out.println(">> Telefone do cliente " + cliente.getIdCliente() + " cadastrado incorretamente. "
+						+ "Insira novamente!");
+			}
+		}
+		
+		System.out.println("\n");
+	}
+	
+	public Cliente consultarCliente(int idCliente) {
+		for(Cliente c : this.clientes) {
+			if(c.getIdCliente() == idCliente) {
+				Cliente cliente = c;
+				return cliente;
+			}
+		}
+		return null;
+	}
+	
+	public void listarClientes() {
+		
+		System.out.println(">>> Clientes cadastrados:");
+		
+		for(Cliente c : this.clientes) {
+			System.out.println("- - - - - - - - - -");
+			System.out.println(">> Cliente ID=" + c.getIdCliente() + ": ");
+			System.out.println("> Nome: " + c.getNome());
+			System.out.println("> CPF: " + c.getCpf());
+			System.out.println("> Endereço: " + c.getEndereço());
+			System.out.println("> Telefone: " + c.getTelefone());
+		}
+		
+		System.out.println("\n");
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
+import java.util.Date;
+
+import BUSINESS.Cliente;
+import BUSINESS.Fornecedor;
+import BUSINESS.MateriaPrima;
+import BUSINESS.ClienteService;
 
 public class Main {
 
@@ -6,8 +12,22 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("Hello, world!");
+		System.out.print("//=== Bem-vindo ao SmartStock! ===//\n\n");
+		
+		//==== Teste ClienteService ===//
+		System.out.print(">>> Cadastrando o cliente 1...\n");
+		Cliente cliente1 = new Cliente(1, "Maria", "123", "Rua Palmares, 180", "84-3332222");
+		
+		System.out.print(">>> Cadastrando o cliente 2...\n");
+		Cliente cliente2 = new Cliente(2, "João", "11122233344", "Rua dos Poetas, 255", "84-44445555");
+		
+		Cliente []clientes = {cliente1, cliente2};		
+		ClienteService serviçoCliente = new ClienteService();
+		serviçoCliente.setCliente(clientes);
+		serviçoCliente.validarCadastroCliente(1);
+		serviçoCliente.listarClientes();
+		System.out.println(">>> Consulta nome cliente 2: " + serviçoCliente.consultarCliente(2).getNome() + "\n");
+		//=============================//
 
 	}
 
