d9ae1e70405b27bf815c801574b1c1dba6bf4160
Yago Marques <yagobsmarques@gmail.com>
2021-02-16 12:35:11 -0300

    implementando as primeiras funções de produto DAO

--- src/DATA/ProdutoFinalDAO.java
+++ src/DATA/ProdutoFinalDAO.java
@@ -1,9 +1,38 @@
 package DATA;
 
+import java.util.ArrayList;
+
+import BUSINESS.ProdutoFinal;
+
 public class ProdutoFinalDAO {
+	ArrayList<ProdutoFinal> produtos;
 
 	public ProdutoFinalDAO() {
 		// TODO Auto-generated constructor stub
+		this.produtos = new ArrayList<ProdutoFinal>();
 	}
 
+	public void inserir(ProdutoFinal produto) {
+		this.produtos.add(produto);
+	}
+
+	public void remover(ProdutoFinal produto) {
+		this.produtos.remove(produto);
+	}
+
+	public void alterar() {
+		//TODO things
+	}
+
+	public ProdutoFinal procuraPeloId(int id) {
+		for (ProdutoFinal produto : this.produtos) {
+			if (produto.getIdProdutoFinal() == id) {
+				return produto;
+			}
+		}
+		return null;
+	}
+	public ArrayList<ProdutoFinal> procuraTodos(){
+		return this.produtos;
+	}
 }
