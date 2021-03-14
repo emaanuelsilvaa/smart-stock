6c16d8b993228ce0763e2893c931aa933d22ed0c
Camila Duarte <camiladuartes_@ufrn.edu.br>
2021-03-14 20:27:38 -0300

    BusinessRuleException class

---
+++ src/UTIL/BusinessRuleException.java
@@ -0,0 +1,11 @@
+package UTIL;
+
+public class BusinessRuleException extends Exception {
+	public BusinessRuleException() {
+		super();
+	}
+	
+	public BusinessRuleException(String message) {
+		super(message);
+	}
+}
