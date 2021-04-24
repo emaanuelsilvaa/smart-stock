716b310bf2f927065397938df71218ed5cc262a1
Camila Duarte <camiladuartes_@ufrn.edu.br>
2021-04-24 12:12:54 -0300

    especificidades venda/encomenda de alimento

--- src/BUSINESS/ValidarEspecificidadesVendaAlimento.java
+++ src/BUSINESS/ValidarEspecificidadesVendaAlimento.java
@@ -3,14 +3,30 @@ package BUSINESS;
 import java.util.ArrayList;
 
 import ENTITY.Especificidade;
+import ENTITY.EspecificidadeAlimento;
 import ENTITY.Venda;
 
 public class ValidarEspecificidadesVendaAlimento implements EspecificidadeVendaStrategy {
+	
+	public ValidarEspecificidadesVendaAlimento() {
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
 	public ArrayList<String> validarEspecificidades(Especificidade especificidade, Venda venda) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<String> erros = new ArrayList<String>();
+		boolean ehParaBuffetCadastrado = ((EspecificidadeAlimento) especificidade).isBuffetCadastrado();
+		if(ehParaBuffetCadastrado) {
+			float desconto = ((EspecificidadeAlimento) especificidade).getFatorDeDesconto();
+			if(desconto < 0 || desconto > 1) {
+				erros.add("Fator de desconto inválido");
+			}
+			else {
+				float valor = venda.getValor();
+				venda.setValor(valor * (1-desconto));
+			}
+		}
+		return erros;
 	}
 
 }
--- src/ENTITY/EspecificidadeAlimento.java
+++ src/ENTITY/EspecificidadeAlimento.java
@@ -1,9 +1,28 @@
 package ENTITY;
 
 public class EspecificidadeAlimento extends Especificidade {
-
+	
+	protected boolean buffetCadastrado; // informa se é para um buffet cadastrado
+	protected float fatorDeDesconto; // caso seja, tem fator de desconto
+	
 	public EspecificidadeAlimento() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public boolean isBuffetCadastrado() {
+		return buffetCadastrado;
+	}
+
+	public void setBuffetCadastrado(boolean buffetCadastrado) {
+		this.buffetCadastrado = buffetCadastrado;
+	}
+
+	public float getFatorDeDesconto() {
+		return fatorDeDesconto;
+	}
+
+	public void setFatorDeDesconto(float fatorDeDesconto) {
+		this.fatorDeDesconto = fatorDeDesconto;
 	}
 	
 }
