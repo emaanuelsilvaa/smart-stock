33ceed9a7c17b6ce2060a5bb9c6fec3b2addabdf
Gerson David da Silva Santos <gds_jimmy@hotmail.com>
2022-02-10 22:53:45 -0300

    Adicionadas especificações para: EncomendaService (construtor, getInstance, validarCadastro), IEncomendaService (procuraTodos, procuraPeloId, validarCadastro)

--- src/BUSINESS/IEncomendaService.java
+++ src/BUSINESS/IEncomendaService.java
@@ -8,10 +8,20 @@ import ENTITY.Encomenda;
 import UTIL.BusinessRuleException;
 
 public interface IEncomendaService {
-
+	//@ public model instance ArrayList<Encomenda> encomendas;
+	//@ public model instance Encomenda encomenda;
+	
+	//@ ensures encomendas.equals(\result);
 	ArrayList<Encomenda> procuraTodos();
 
-	Encomenda procuraPeloId(int id);
+	/*@ assignable encomenda;
+	 @  ensures encomenda != null;
+	 @	ensures encomenda.getId() == id;
+	 @  also
+	 @  assignable encomenda;
+	 @  ensures encomenda == null;
+	@*/
+	Encomenda procuraPeloId (int id);
 
 	int inserir(Encomenda encomenda) throws BusinessRuleException;
 
@@ -21,8 +31,25 @@ public interface IEncomendaService {
 
 	int alterar(int id, Encomenda encomenda) throws BusinessRuleException;
 
+	// /*@ ensures encomenda.getId() == \result;
+	//  @  ensures encomenda.getIdCliente() == idCliente;
+	//  @  ensures encomenda.getListaProdutos().equals(listaProdutos);
+	//  @  ensures encomenda.getDataEntrega().compareTo(dataEntrega) == 0;
+	// @*/
 	int realizarEncomenda(HashMap<Integer, Integer> listaProdutos, int idCliente, Date dataEntrega);
 
+	/*@ requires encomenda != null;
+	 @  requires encomenda.getValor() > 0;
+	 @  requires encomenda.getIdCliente() >= 1;
+	 @  requires !encomenda.getListaProdutos().isEmpty();
+	 @  ensures \result == 0;
+	 @  also
+	 @  requires encomenda == null;
+	 @  requires encomenda.getValor() <= 0;
+	 @  requires encomenda.getIdCliente() < 1;
+	 @  requires encomenda.getListaProdutos().isEmpty();
+	 @  signals_only BusinessRuleException;
+	@*/
 	int validarCadastro(Encomenda encomenda) throws BusinessRuleException;
 
 }
